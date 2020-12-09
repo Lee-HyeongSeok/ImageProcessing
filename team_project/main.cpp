568fd2b4ac4b02acfa9ce4e94e8e7f4331d37ece
leehyeongseck <kea7109@naver.com>
2020-12-10 01:34:31 +0900

    자율주행 자동차 영상

--- team_project/main.cpp
+++ team_project/main.cpp
@@ -4,11 +4,17 @@
 using namespace std;
 using namespace cv;
 
+void VideoError(VideoCapture cap) {
+	if (!cap.isOpened()) {
+		printf("video error");
+		return;
+	}
+}
 int main() {
-	VideoCapture cap("D:/3-2학기/영상처리-안세종/프로젝트 영상/주간도로주행.mp4");
-	if (!cap.isOpened())
-		return -1;
+	VideoCapture cap("D:/3-2학기/영상처리-안세종/프로젝트 영상/anew.mp4");
+	VideoError(cap);
 
+	
 	/*
 	1. 캐니
 	2. 이진화
@@ -16,28 +22,60 @@ int main() {
 	4. 마스크
 	5. 영상 합성
 	*/
-	for (;;) {
-		Mat dst, convert_color;
-		Mat frame;
-		cap >> frame;
-		Canny(frame, dst, 100, 200); // 캐니 에지 검ㅁ출 
-		imshow("canny", dst);
+	Mat dst, convert_color;
+	Mat frame;
+	Mat fin;
 	
-		cvtColor(dst, convert_color, CV_GRAY2BGR);
-		imshow("convert", convert_color);
+	for (;;) {
+		cap >> frame;
+
+		// 캐니 에지 검출 
+		Canny(frame, dst, 100, 200); 
+
+		// 마스크 처리 
+		Mat mask = Mat::zeros(dst.size(), dst.type());
+		vector<Point> points;
+		points.push_back(Point(dst.cols / 2.2, dst.rows / 2.0));
+		points.push_back(Point((dst.cols / 2.1), dst.rows / 2.0));
+		points.push_back(Point(dst.cols/1.5, dst.rows));
+		points.push_back(Point(dst.cols / 8.8, dst.rows));
+		fillConvexPoly(mask, points, Scalar(255, 255, 255));
+		
+		bitwise_and(dst, mask, fin);
+		imshow("mask", mask);
+		
 		vector<Vec4i> lines;
-		HoughLinesP(dst, lines, 1, CV_PI / 180, 100, 100, 10);
+		HoughLinesP(fin, lines, 1, CV_PI / 180, 50, 55, 5);
+
+		// 화소 색 변경 
+		cvtColor(fin, convert_color, CV_GRAY2BGR);
 		for (size_t i = 0; i < lines.size(); i++) {
 			Vec4i l = lines[i];
+			line(fin, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 1, CV_AA);
 			line(convert_color, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 1, CV_AA);
+			line(frame, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 1, CV_AA);
 		}
+		
+		
 		resize(frame, frame, Size(600, 400));
-		resize(convert_color, convert_color, Size(600, 400));
+		resize(dst, dst, Size(600, 400));
+		resize(fin, fin, Size(600, 400));
+		imshow("convert", convert_color);
+		imshow("origin", frame);
+		imshow("fin", fin);
+		imshow("dst", dst);
+		
+		/*
+		imshow("fin", fin);
+		
+		imshow("mask", mask);
 		imshow("frame", frame);
 		imshow("dst", convert_color);
+		*/
 		if (waitKey(30) >= 0)
 			break;
 	}
+	
 	waitKey(0);
 	return 0;
 }
